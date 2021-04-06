afa15bf3d6534d34d4a4fb9fac1f2a2715845598
Aleksandr <shohin-sasha1993@yandex.ru>
2021-04-06 15:21:59 +0300

    Add AbstractArrow and added to it three classes of heirs

---
+++ UMLDiagram/Arrows/AbstractArrow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace UMLDiagram.Arrows
+{
+    public abstract class AbstractArrow
+    {
+        Point StartPoint { get; set; }
+        Point EndPoint { get; set; }
+        public abstract void Draw(Graphics graphics);
+
+
+        
+        
+    }
+}
