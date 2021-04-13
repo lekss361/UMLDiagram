11eeb22886cccdb36146dfa700dbcb22899ed8d5
Yanzhit <yanzhit@gmail.com>
2021-04-13 20:18:43 +0300

    Move method СalculationOfAngles on AbstractArrow

--- UMLDiagram/Arrows/AbstractArrow.cs
+++ UMLDiagram/Arrows/AbstractArrow.cs
@@ -1,5 +1,6 @@
-﻿using System.Drawing;
-using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
 
 namespace UMLDiagram.Arrows
 {
@@ -14,15 +15,45 @@ namespace UMLDiagram.Arrows
 
         public double arrow_degrees { get; set; }// размах крыльев или острота угла 
 
-        public double x1 { get; set; }
-        public double y1 { get; set; }
-        public double x2 { get; set; }
-        public double y2 { get; set; }
-        public double x3 { get; set; }
-        public double y3 { get; set; }
-        public double x4 { get; set; }
-        public double y4 { get; set; }
+        //public double x1 { get; set; }
+        //public double y1 { get; set; }
+        //public double x2 { get; set; }
+        //public double y2 { get; set; }
+        //public double x3 { get; set; }
+        //public double y3 { get; set; }
+        //public double x4 { get; set; }
+        //public double y4 { get; set; }
 
+        public List<Point> GetPoints()
+        {
+            List<Point> points = new List<Point>();
+
+            arrow_lenght = 15;
+            arrow_degrees = 300;
+
+            double x1, y1, x2, y2, x3, y3, x4, y4;
+
+            int hightTriangle = Convert.ToInt32(arrow_lenght * Math.Sqrt(3) / 2);
+
+            angle = Math.Atan2(EndPoint.Y - StartPoint.Y, EndPoint.X - StartPoint.X) + Math.PI; // угол поворота линии
+
+            x1 = EndPoint.X + arrow_lenght * Math.Cos(angle - arrow_degrees);
+            y1 = EndPoint.Y + arrow_lenght * Math.Sin(angle - arrow_degrees);
+            x2 = EndPoint.X + arrow_lenght * Math.Cos(angle + arrow_degrees);
+            y2 = EndPoint.Y + arrow_lenght * Math.Sin(angle + arrow_degrees);
+            x3 = EndPoint.X - 30 * Math.Cos(angle);
+            y3 = EndPoint.Y - 30 * Math.Sin(angle);
+
+            x4 = EndPoint.X + 30 * Math.Cos(angle);
+            y4 = EndPoint.Y + 30 * Math.Sin(angle);
+
+            points.Add(new Point(Convert.ToInt32(x1), Convert.ToInt32(y1)));
+            points.Add(new Point(Convert.ToInt32(x2), Convert.ToInt32(y2)));
+            points.Add(new Point(Convert.ToInt32(x3), Convert.ToInt32(y3)));
+            points.Add(new Point(Convert.ToInt32(x4), Convert.ToInt32(y4)));
+
+            return points;
+        }
 
         public abstract void Draw( Graphics graphics, Pen pen);
 
--- UMLDiagram/Arrows/CompositionArrow.cs
+++ UMLDiagram/Arrows/CompositionArrow.cs
@@ -18,47 +18,55 @@ namespace UMLDiagram.Arrows
 
         public void DrawAggregationArrow(Graphics graphics, Pen pen)
         {
-            Point x1y1, x2y2, x3y3, x4y4;
-            СalculationOfAngles(EndPoint, StartPoint, out x1y1, out x2y2, out x3y3, out x4y4);
+            //Point x1y1, x2y2, x3y3, x4y4;
+            //СalculationOfAngles(EndPoint, StartPoint, out x1y1, out x2y2, out x3y3, out x4y4);
 
             graphics.DrawLine(pen, EndPoint, StartPoint); // тут рисуем линию 
-            graphics.DrawLine(pen, x3y3, x1y1);
-            graphics.DrawLine(pen, x3y3, x2y2);
+            //graphics.DrawLine(pen, x3y3, x1y1);
+            //graphics.DrawLine(pen, x3y3, x2y2);
 
-            graphics.DrawLine(pen, x1y1, x4y4);
-            graphics.DrawLine(pen, x2y2, x4y4);
+            //graphics.DrawLine(pen, x1y1, x4y4);
+            //graphics.DrawLine(pen, x2y2, x4y4);
+
+            Point[] capPoints = GetPoints().ToArray();
+            graphics.DrawLine(pen, capPoints[2], capPoints[0]);
+            graphics.DrawLine(pen, capPoints[2], capPoints[1]);
+            graphics.DrawLine(pen, capPoints[0], capPoints[3]);
+            graphics.DrawLine(pen, capPoints[1], capPoints[3]);
 
             SolidBrush solidBrush = new SolidBrush(pen.Color); // кисть для закрашивания треуголника
-            Point[] pointF = new Point[] { x3y3, x2y2, x1y1, x4y4, x2y2, x1y1}; // массив точек для закрашивания треугольника 
+            //Point[] pointF = new Point[] { x3y3, x2y2, x1y1, x4y4, x2y2, x1y1}; // массив точек для закрашивания треугольника 
+            Point[] pointF = new Point[] { capPoints[2], capPoints[1], capPoints[0], capPoints[3], capPoints[1], capPoints[0] }; // массив точек для закрашивания треугольника 
             graphics.FillPolygon(solidBrush, pointF); // закрашиваем треуголник 
+            //graphics.FillPolygon(solidBrush, capPoints); // закрашиваем треуголник 
 
         }
 
 
-        protected void СalculationOfAngles(Point mCur, Point mStart, out Point x1y1, out Point x2y2, out Point x3y3, out Point x4y4)
-        {
-            arrow_lenght = 15;
-            arrow_degrees = 300;
-            int hightTriangle = Convert.ToInt32(arrow_lenght * Math.Sqrt(3) / 2);
+        //protected void СalculationOfAngles(Point mCur, Point mStart, out Point x1y1, out Point x2y2, out Point x3y3, out Point x4y4)
+        //{
+        //    arrow_lenght = 15;
+        //    arrow_degrees = 300;
+        //    int hightTriangle = Convert.ToInt32(arrow_lenght * Math.Sqrt(3) / 2);
 
-            angle = Math.Atan2(mCur.Y - mStart.Y, mCur.X - mStart.X) + Math.PI; // угол поворота линии
+        //    angle = Math.Atan2(mCur.Y - mStart.Y, mCur.X - mStart.X) + Math.PI; // угол поворота линии
 
-            x1 = mCur.X + arrow_lenght * Math.Cos(angle - arrow_degrees);
-            y1 = mCur.Y + arrow_lenght * Math.Sin(angle - arrow_degrees);
+        //    x1 = mCur.X + arrow_lenght * Math.Cos(angle - arrow_degrees);
+        //    y1 = mCur.Y + arrow_lenght * Math.Sin(angle - arrow_degrees);
 
-            x2 = mCur.X + arrow_lenght * Math.Cos(angle + arrow_degrees);
-            y2 = mCur.Y + arrow_lenght * Math.Sin(angle + arrow_degrees);
+        //    x2 = mCur.X + arrow_lenght * Math.Cos(angle + arrow_degrees);
+        //    y2 = mCur.Y + arrow_lenght * Math.Sin(angle + arrow_degrees);
 
-            x3 = mCur.X - 30 * Math.Cos(angle);
-            y3 = mCur.Y - 30 * Math.Sin(angle);
+        //    x3 = mCur.X - 30 * Math.Cos(angle);
+        //    y3 = mCur.Y - 30 * Math.Sin(angle);
 
-            x4 = mCur.X + 30 * Math.Cos(angle);
-            y4 = mCur.Y + 30 * Math.Sin(angle);
+        //    x4 = mCur.X + 30 * Math.Cos(angle);
+        //    y4 = mCur.Y + 30 * Math.Sin(angle);
 
-            x1y1 = new Point(Convert.ToInt32(x1), Convert.ToInt32(y1));
-            x2y2 = new Point(Convert.ToInt32(x2), Convert.ToInt32(y2));
-            x3y3 = new Point(Convert.ToInt32(x3), Convert.ToInt32(y3));
-            x4y4 = new Point(Convert.ToInt32(x4), Convert.ToInt32(y4));
-        }
+        //    x1y1 = new Point(Convert.ToInt32(x1), Convert.ToInt32(y1));
+        //    x2y2 = new Point(Convert.ToInt32(x2), Convert.ToInt32(y2));
+        //    x3y3 = new Point(Convert.ToInt32(x3), Convert.ToInt32(y3));
+        //    x4y4 = new Point(Convert.ToInt32(x4), Convert.ToInt32(y4));
+        //}
     }
 }
