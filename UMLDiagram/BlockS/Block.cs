5ccfe74a0332f3192a441239a792236362e16b68
Katerina <k.panteleewa@gmail.com>
2021-04-25 23:04:00 +0300

    deleted unused comments

--- UMLDiagram/BlockS/Block.cs
+++ UMLDiagram/BlockS/Block.cs
@@ -10,24 +10,13 @@ namespace UMLDiagram.BlockS
 {
     public class Block : AbstractBlock
     {
-        //static float width;
-        //static float heigth;
+      
 
         static Point _startName;
         static Point _startAtr;
         static Point _startMet;
 
-        //public static void SetPointForLines(Point st, float heigth, int cAtrL, int cMetL)
-        //{
-        //    _startName.X = st.X + 10;
-        //    _startName.Y = st.Y;
-
-        //    _startAtr.X = st.X + 10;
-        //    _startAtr.Y = st.Y + (int)heigth + 20 ;
-           
-        //    _startMet.Y = st.Y  + (int)heigth + 20 + (cAtrL * (int)heigth + 20);
-        //    _startMet.X = st.X + 10;
-        //}
+      
         public void SetPointForLines(Point st )
         {
             _startName.X = st.X + 10;
@@ -49,35 +38,16 @@ namespace UMLDiagram.BlockS
         {
             SetPointForLines(st);
         }
-        //public override void DrawBlock(Graphics graphics, Pen pen, Point start, string name, string atr,string met,float width,float heigth, int cAtrL, int cMetL)
-        //{
-
-        //    float mult = (cAtrL + cMetL) * heigth + 60;
-
-
-
-        //    graphics.DrawRectangle(pen, start.X, start.Y,width+10, mult); //основной 
-        //    graphics.DrawRectangle(pen, start.X, start.Y, width+10 , heigth+10); //heigth+10 - высота с именем  
-
-        //    Font drawFont = new Font("Arial", 8);
-        //    SolidBrush drawBrush = new SolidBrush(Color.Black);
-
-
-        //    graphics.DrawString(name, drawFont, drawBrush, _startName.X , _startName.Y);
-        //    graphics.DrawString(atr, drawFont, drawBrush, _startAtr.X, _startAtr.Y );
-        //    graphics.DrawString(met, drawFont, drawBrush, _startMet.X , _startMet.Y );
-
-        //}
-        //public override void DrawBlock(Graphics graphics, Pen pen, Point start)
+        
         public override void Draw(Graphics graphics, Pen pen)
         {
             Point start = startPoint;
-            //float totaLength = (NumOfAtributeLines + NumOfMethodLines) * HeigthFont + 60;
+            
             float totaLength = ((NumOfAtributeLines + NumOfMethodLines) * HeigthFont) + 80;
             graphics.DrawRectangle(pen, start.X, start.Y, WidthRect + 10, totaLength); //основной 
             graphics.DrawRectangle(pen, start.X, start.Y, WidthRect + 10, HeigthFont + 10); //heigth+10 - высота с именем  
 
-            //Font drawFont = new Font("Arial", 8);
+            
             SolidBrush drawBrush = new SolidBrush(Color.Black);
 
             graphics.DrawString(NameField, drawFont, drawBrush, _startName.X, _startName.Y);
